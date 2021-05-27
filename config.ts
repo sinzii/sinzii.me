f1af6a40412347acf8d5c1f3a153705f7adfda79
sinzii <thangvx@newayict.com>
2021-05-28 02:03:03 +0700

    Move side projects info to config file

--- config.ts
+++ config.ts
@@ -7,5 +7,16 @@ export default {
     siteDescription: 'Where my thoughts wander...',
     dateTimeFormat: 'DD/MM/YYYY - HH:mm',
     dateFormat: 'DD/MM/YYYY',
-    postUrlStyle: PostUrlStyle.YEAR_MONTH_SLUG
+    postUrlStyle: PostUrlStyle.YEAR_MONTH_SLUG,
+    me: {
+        sideProjects: [
+            {
+                name: '<strong>meblog</strong> - A DIY blog engine',
+                url: 'https://github.com/sinzii/meblog'
+            }, {
+                name: '<strong>frameworkless</strong> - A frameworkless NodeJS application',
+                url: 'https://github.com/sinzii/frameworkless'
+            }
+        ]
+    }
 } as Config
