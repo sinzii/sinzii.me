d5ff083cdf4a9e1693ac4ee0e82de793c827306c
Thang X. Vu <thang@coongcrafts.io>
2024-06-28 20:38:49 +0700

    chore: update main info

--- config.js
+++ config.js
@@ -12,13 +12,26 @@ module.exports = {
         github: 'https://github.com/sinzii',
         sideProjects: [
             {
+                name: '<strong>Dedot</strong> - A delightful JavaScript/TypeScript client for <strong>Polkadot</strong> & <strong>Substrate</strong>',
+                url: 'https://github.com/dedotdev/dedot'
+            },
+            {
+                name: '<strong>InSpace</strong> - A protocol to launch and manage your community spaces using <strong>ink!</strong> smart contracts',
+                url: 'https://github.com/CoongCrafts/inspaciness'
+            },
+            {
+                name: '<strong>Coong Wallet</strong> - A website-based multi-chain crypto wallet for <strong>Polkadot</strong> & <strong>Kusama</strong> ecosystem',
+                url: 'https://github.com/CoongCrafts/coong-wallet'
+            },
+            {
                 name: '<strong>meblog</strong> - A simple blog engine for personal blogging',
                 url: 'https://github.com/sinzii/meblog'
-            }, {
+            },
+            {
                 name: '<strong>frameworkless</strong> - A frameworkless NodeJS application',
                 url: 'https://github.com/sinzii/frameworkless'
             }
         ]
     },
-    predefinedTags: ['javascript', 'nodejs', 'frontend']
+    predefinedTags: ['javascript', 'nodejs', 'frontend', 'blockchain', 'polkadot', 'substrate']
 }
